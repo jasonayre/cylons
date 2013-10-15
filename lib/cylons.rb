894669e08f6c9cd75713546ddc7afd2414408160
Jason Ayre <jasonayre@gmail.com>
2013-10-14 19:19:46 -0600

    demonstrate dcell issue with launching ghost threads without being initialized, or something...

--- lib/cylons.rb
+++ lib/cylons.rb
@@ -7,53 +7,51 @@ require 'active_support/json'
 require 'will_paginate'
 require "cylons/version"
 
-require 'cylons/attributes'
-require 'cylons/connection'
-require 'cylons/configuration'
-require 'cylons/errors'
-# require 'cylons/search'
-# require 'cylons/rpc'
-require 'cylons/remote'
-require 'cylons/remote_registry'
-require 'cylons/remote_discovery'
-require 'cylons/remote_proxy'
-require 'cylons/registry_adapter'
-require 'cylons/service'
-require 'cylons/service_manager'
+# require 'cylons/attributes'
+# require 'cylons/connection'
+# require 'cylons/configuration'
+# require 'cylons/errors'
+# require 'cylons/remote'
+# require 'cylons/remote_registry'
+# require 'cylons/remote_discovery'
+# require 'cylons/remote_proxy'
+# require 'cylons/registry_adapter'
+# require 'cylons/service'
+# require 'cylons/service_manager'
 require 'dcell'
 require 'socket'
-require 'zk'
-require 'dcell/registries/zk_adapter'
-require 'cylons/railtie'
+# require 'zk'
+# require 'dcell/registries/zk_adapter'
+# require 'cylons/railtie'
 require 'pry'
 # require 'cylons/railtie' if defined?(Rails)
 
 module Cylons
   #dont load remotes if test env or SKIP_CYLONS=true (such as when running rake tasks)  
-  def self.skip_cylons?
-    !!ENV["SKIP_CYLONS"]
-  end
-    
-  def self.silence?
-    skip_cylons? || (defined?(Rails) && Rails.env == "test")
-  end
-  
-  class << self
-    attr_accessor :configuration, :logger
-    
-    def configuration
-      @configuration ||= ::Cylons::Configuration.new
-    end
-
-    def configure
-      yield(configuration) if block_given?
-      
-      @logger = configuration.logger
-      
-      ::ActiveSupport.run_load_hooks(:cylons, self)
-    end
-    
-    alias_method :config, :configuration
-  end
+  # def self.skip_cylons?
+  #   !!ENV["SKIP_CYLONS"]
+  # end
+  #   
+  # def self.silence?
+  #   skip_cylons? || (defined?(Rails) && Rails.env == "test")
+  # end
+  # 
+  # class << self
+  #   attr_accessor :configuration, :logger
+  #   
+  #   def configuration
+  #     @configuration ||= ::Cylons::Configuration.new
+  #   end
+  # 
+  #   def configure
+  #     yield(configuration) if block_given?
+  #     
+  #     @logger = configuration.logger
+  #     
+  #     ::ActiveSupport.run_load_hooks(:cylons, self)
+  #   end
+  #   
+  #   alias_method :config, :configuration
+  # end
 end
 
